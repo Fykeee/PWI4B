8ff0a9de5f44837637309d434e772984c11504b5
Filip Mikołajczak <121062871+Fykeee@users.noreply.github.com>
2023-11-12 01:41:28 +0100

    Add files via upload

---
+++ 2.11/panel/login.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+if(isset($_SESSION['admin_name'])){
+    header('Location: ./');
+    die();
+}
+
+if(isset($_POST['login'], $_POST['username'], $_POST['pass'])){
+    include '../include/db.php';
+
+    $username = $_POST['username'];
+    $pass = $_POST['pass'];
+
+    $adminData = findAdminByLogin($username);
+
+    if($adminData){
+        $hashedPassword = hash('sha256', $pass);
+
+        if($hashedPassword === $adminData['password']){
+            $_SESSION['admin_name'] = $adminData['name'];
+
+            header('Location ./');
+
+            die;
+        }
+    }
+}
+
+$title='Panel administracyjny | Logowanie:';
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<?php
+require_once '../include/head.php';
+?>
+<div class="container-fluid p-0">
+    <div class="container py-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 col-md-6">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>Zaloguj się do panelu</h1>
+                        <!--<?= hash('sha256', 'admin') ?>-->
+                    </div>
+                    <div class="card-body">
+                        <form method="post">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Login</label>
+                                <input type="text" class="form-control" id="username" name="username" required autocomplete="off">
+                            </div>
+                            <div class="mb-3">
+                                <label for="pass" class="form-label">Hasło</label>
+                                <input type="password" class="form-control" id="pass" name="pass" required autocomplete="off">
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="login">
+                                Zaloguj się
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
